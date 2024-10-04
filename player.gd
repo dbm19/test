f46bb39af80588f894e193f35218e47e06133144
Daniel Mau <daniel.b.mau@gmail.com>
2024-10-04 14:14:18 -0400

    Added explosion timing

--- player.gd
+++ player.gd
@@ -1,11 +1,23 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var player_range
+var player_sprite
+var vanish_timer
 
-func get_input():
-	var input_direction = Input.get_vector("go_left", "go_right", "go_up", "go_down")
-	velocity = input_direction * speed
+func _ready():
+	player_range = get_node("PlayerRange")
+	player_sprite = get_node("PlayerSprite")
+	vanish_timer = get_node("VanishTimer")
 
-func _physics_process(delta):
-	get_input()
-	move_and_slide()
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_timer_timeout() -> void:
+	player_range.visible = true
+	player_sprite.visible = false
+	vanish_timer.start()
+
+func _on_vanish_timer_timeout() -> void:
+	queue_free()
