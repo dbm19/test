b57ab119c2b251f10502f4a655ac693f55d3655f
Daniel Mau <daniel.b.mau@gmail.com>
2024-10-04 14:40:24 -0400

    Added explosion functionality

--- player.gd
+++ player.gd
@@ -4,11 +4,13 @@ extends CharacterBody2D
 var player_range
 var player_sprite
 var vanish_timer
+var player_range_collision
 
 func _ready():
 	player_range = get_node("PlayerRange")
 	player_sprite = get_node("PlayerSprite")
 	vanish_timer = get_node("VanishTimer")
+	player_range_collision = get_node("PlayerRange/PlayerRangeArea/PlayerRangeCollisionShape")
 
 func _process(delta: float) -> void:
 	pass
@@ -18,6 +20,12 @@ func _on_player_timer_timeout() -> void:
 	player_range.visible = true
 	player_sprite.visible = false
 	vanish_timer.start()
+	player_range_collision.set_deferred("disabled", false)
 
 func _on_vanish_timer_timeout() -> void:
 	queue_free()
+
+func _on_player_range_area_area_entered(area: Area2D) -> void:
+	if area.get_groups().size() > 0:
+		if area.is_in_group("mob"):
+			area.queue_free()
