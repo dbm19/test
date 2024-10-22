5bf75eaf1600d3e698ab83ba5858ab80d8186490
Daniel Mau <daniel.b.mau@gmail.com>
2024-10-22 10:20:48 -0400

    Added damage indication for mob

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -2,10 +2,12 @@ extends Area2D
 
 var direction
 var speed = 100
-var hit_points = 1
+var hit_points = 2
+var mob_sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	mob_sprite = get_node("MobSprite")
 	Global.mob_spawn = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +22,6 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("bullet"):
 		hit_points -= 1
 		area.queue_free()
+		mob_sprite.texture = load("res://art/mob_sprite_damaged.png")
+		await get_tree().create_timer(0.18).timeout
+		mob_sprite.texture = load("res://art/mob_sprite.png")
